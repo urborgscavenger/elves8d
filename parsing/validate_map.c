2546266974779835575ff14e5c1617e233074315
Wiktoria Lisson <wilisson@2-C-2.42heilbronn.de>
2026-03-23 21:46:57 +0100

    fixed norm in validate_map

--- parsing/validate_map.c
+++ parsing/validate_map.c
@@ -6,45 +6,63 @@
 /*   By: wilisson <wilisson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 12:00:00 by wilisson          #+#    #+#             */
-/*   Updated: 2026/03/16 14:33:19 by wilisson         ###   ########.fr       */
+/*   Updated: 2026/03/23 21:44:42 by wilisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
+static int	handle_floor_cell(t_flood_params *params, int x, int y,
+			int *found_floor)
+{
+	*found_floor = 1;
+	if (flood_fill(params, x, y) != 0)
+	{
+		ft_putstr_fd("Error\nMap is not closed/", 2);
+		ft_putstr_fd("surrounded by walls\n", 2);
+		return (-1);
+	}
+	return (0);
+}
+
+static int	scan_grid(t_flood_params *params, char **copy, int *found_floor)
+{
+	int			i;
+	int			j;
+
+	i = 0;
+	while (i < params->height)
+	{
+		j = 0;
+		while (j < params->width)
+		{
+			if (copy[i][j] == '0')
+			{
+				if (handle_floor_cell(params, j, i, found_floor) != 0)
+					return (-1);
+			}
+			++j;
+		}
+		++i;
+	}
+	return (0);
+}
+
 static int	run_flood_check(t_map *map, char **copy)
 {
 	t_flood_params	params;
-	int				i;
-	int				j;
 	int				found_floor;
 
 	params.grid = copy;
 	params.width = map->width;
 	params.height = map->height;
 	found_floor = 0;
-	i = 0;
-	while (i < map->height)
-	{
-		j = 0;
-		while (j < map->width)
-		{
-			if (copy[i][j] == '0')
-			{
-				found_floor = 1;
-				if (flood_fill(&params, j, i) != 0)
-				{
-					ft_putstr_fd("Error\nMap is not closed/surrounded by walls\n", 2);
-					return (-1);
-				}
-			}
-			++j;
-		}
-		++i;
-	}
+	if (scan_grid(&params, copy, &found_floor) != 0)
+		return (-1);
 	if (!found_floor)
 	{
-		ft_putstr_fd("Error\nMap has no open floor cell\n", 2);
+		ft_putstr_fd("Error\nMap has no open ", 2);
+		ft_putstr_fd("floor cell\n", 2);
 		return (-1);
 	}
 	return (0);
